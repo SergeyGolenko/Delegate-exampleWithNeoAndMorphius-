72a64abac9867e7a9ffa7b617e06ee969d32331d
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-14 13:31:17 +0200

    добавил картинку в FirstVC

--- Delegate(exampleWithNeoAndMorphius)/ViewController.swift
+++ Delegate(exampleWithNeoAndMorphius)/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var morpheus = UIImage(named: "morpheus")
+    @IBOutlet weak var imageFirstVC: UIImageView!
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        loadMorphiusPicture()
+    }
+    
+    
+    
+    func loadMorphiusPicture(){
+        self.imageFirstVC.image = morpheus
     }
 
 
