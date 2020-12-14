56336440bb0b1419097e49241596e5ca4da4fa30
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-14 13:44:28 +0200

    добавил кнопку "выбери таблетку"

--- Delegate(exampleWithNeoAndMorphius)/ViewController.swift
+++ Delegate(exampleWithNeoAndMorphius)/ViewController.swift
@@ -10,20 +10,34 @@ import UIKit
 class ViewController: UIViewController {
     
     var morpheus = UIImage(named: "morpheus")
+    
+    
     @IBOutlet weak var imageFirstVC: UIImageView!
     
-   
-
+    @IBAction func chooseApill(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var chooseApill: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         loadMorphiusPicture()
+    updateTitleButton()
+    
     }
     
     
     
     func loadMorphiusPicture(){
         self.imageFirstVC.image = morpheus
+    }
+    
+    func updateTitleButton(){
+        self.chooseApill.titleLabel?.text = "выбери таблетку"
     }
 
 
